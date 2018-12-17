523cf63374f098987e93c497972cedbce5cae537
ThorTaro <tsutsui.sotaro.14@shizuoka.ac.jp>
2018-12-17 18:16:58 +0900

    Initial Commit

---
+++ marvelapi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  marvelapi
+//
+//  Created by Taro on 2018/12/17.
+//  Copyright © 2018 Taro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
